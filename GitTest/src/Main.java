622b59d7d77a7b13bd1d7a1b8b631a84d0ac7c42
rishi <rdholliw@sfu.ca>
2018-01-02 20:21:19 -0800

    initial commit

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello world for git testing");
-//
+		System.out.println("hello world for git testinh");
+
 	}
 
 }
